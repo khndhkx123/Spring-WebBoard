57fb3bb59e57a71ea47a091fe7c48d58fd5f990c
khndhkx123 <khndhkx123@gmail.com>
2020-05-02 15:07:29 +0900

    ADD:regist.jsp / BoardController - Write:GET+POST

--- board/src/main/java/com/kioni/controller/BoardController.java
+++ board/src/main/java/com/kioni/controller/BoardController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;//@Controller
 import org.springframework.ui.Model;//model.addAttribute
 import org.springframework.web.bind.annotation.RequestMapping;//@RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod;//@RequestMethod
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;//@RedirectAttr
 
+import com.kioni.domain.BoardVO;
 import com.kioni.service.BoardService;
 
 @Controller//It is Controller
@@ -19,5 +21,16 @@ public class BoardController {
 	@RequestMapping(value="/listAll", method = RequestMethod.GET)//Access Addr(/listAll)
 	public void listAll(Model model) throws Exception{//Model model : interfaces
 		model.addAttribute("list", service.listAll());//To jsp(service call)
+	}
+	
+	@RequestMapping(value="/regist", method = RequestMethod.GET)
+	public void registerGET(BoardVO board, Model model) throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/regist", method = RequestMethod.POST)
+	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
+		service.regist(board);//Call regist service
+		return "redirect:/listAll";//After POST, return to listAll page
 	}
 }
