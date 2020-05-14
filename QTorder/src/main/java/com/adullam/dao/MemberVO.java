ba3e343a732902efe44a0a6787fb8ebb74b21854
khndhkx123 <khndhkx123@gmail.com>
2020-05-14 21:10:31 +0900

    DB - VO ADDED

---
+++ QTorder/src/main/java/com/adullam/dao/MemberVO.java
@@ -0,0 +1,41 @@
+package com.adullam.dao;
+
+public class MemberVO {
+	/*
+	create table adullam_member (
+	mb_id varchar(50) not null,
+	mb_pw varchar(100) not null,
+	mb_name varchar(30) not null,
+	primary key(mb_id)
+	);
+	*/
+
+	private String mb_id;
+	private String mb_pw;
+	private String mb_name;
+
+	public String getMb_id() {
+		return mb_id;
+	}
+
+	public void setMb_id(String mb_id) {
+		this.mb_id = mb_id;
+	}
+
+	public String getMb_pw() {
+		return mb_pw;
+	}
+
+	public void setMb_pw(String mb_pw) {
+		this.mb_pw = mb_pw;
+	}
+
+	public String getMb_name() {
+		return mb_name;
+	}
+
+	public void setMb_name(String mb_name) {
+		this.mb_name = mb_name;
+	}
+
+}
---
+++ QTorder/src/main/java/com/adullam/dao/QTinfoVO.java
@@ -0,0 +1,41 @@
+package com.adullam.dao;
+
+public class QTinfoVO {
+	/*
+	create table qt_info (
+	qt_no int not null,
+	qt_name varchar(50) not null,
+	qt_price int not null,
+	primary key(qt_no)
+	);
+	*/
+
+	private int qt_no;
+	private String qt_name;
+	private int qt_price;
+
+	public int getQt_no() {
+		return qt_no;
+	}
+
+	public void setQt_no(int qt_no) {
+		this.qt_no = qt_no;
+	}
+
+	public String getQt_name() {
+		return qt_name;
+	}
+
+	public void setQt_name(String qt_name) {
+		this.qt_name = qt_name;
+	}
+
+	public int getQt_price() {
+		return qt_price;
+	}
+
+	public void setQt_price(int qt_price) {
+		this.qt_price = qt_price;
+	}
+
+}
